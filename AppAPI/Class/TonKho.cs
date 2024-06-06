35b62909cceee425de9b4a2ab9cbd0ca1e985f1a
ngoxuanhoan1997github <hoanngo1997@gmail.com>
2024-06-06 15:26:42 +0700

    class ton kho

---
+++ AppAPI/Class/TonKho.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAPI.Class
+{
+    public class TonKho
+    {
+        public string Site { get; set; }
+        public string Ten { get; set; }
+        public string DiaChi { get; set; }
+        public string SLTon { get; set; }
+    }
+}
